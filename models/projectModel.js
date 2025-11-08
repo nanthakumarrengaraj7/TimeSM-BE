548e7aed2bd313fcce1de3188d53aef282ba0f28
nanthakumarrengaraj7 <nanthakumartyson777@gmail.com>
2025-11-09 00:09:20 +0530

    update bcrypt to bcryptjs

--- models/projectModel.js
+++ models/projectModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const projectSchema = new mongoose.Schema({
     projectName: { type: String, require: true },
